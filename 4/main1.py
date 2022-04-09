c48bfec6254fb137b51f1433eff06127a5ba6bc8
ShGulnaz <shakirova139@gmail.com>
2022-04-09 12:12:02 +0300

    4, 5

--- 4/main1.py
+++ 4/main1.py
@@ -35,6 +35,7 @@ indexes = file.readlines()
 json = json5.loads("".join(indexes))
 idf = compute_idf(json)
 tfidf = []
+all_tf = []
 for i in range(1, 101):
     file = open(f'../2/file/{i}.txt', 'r', encoding='utf-8')
     lines = file.readlines()
@@ -44,7 +45,14 @@ for i in range(1, 101):
         p = morph.parse(word)[0].normal_form
         words.append(p)
     tf = compute_tf(words)
+    all_tf.append(tf)
     tfidf.append(compute_tfidf(tf,idf))
 str = json5.dumps(tfidf, separators=(',', ':'), sort_keys=False, indent=4, ensure_ascii=False)
-file = open(f'index.txt', 'w', encoding='utf-8')
+file = open(f'tfidf.txt', 'w', encoding='utf-8')
 file.write(str)
+str1 = json5.dumps(idf, separators=(',', ':'), sort_keys=False, indent=4, ensure_ascii=False)
+file = open(f'idf.txt', 'w', encoding='utf-8')
+file.write(str1)
+str2 = json5.dumps(all_tf, separators=(',', ':'), sort_keys=False, indent=4, ensure_ascii=False)
+file = open(f'tf.txt', 'w', encoding='utf-8')
+file.write(str2)
