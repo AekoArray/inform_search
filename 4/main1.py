42e49c247d174fdebf11c7d4bb9277f1b6328047
ShGulnaz <shakirova139@gmail.com>
2022-04-09 11:15:45 +0300

    4, 5

---
+++ 4/main1.py
@@ -0,0 +1,50 @@
+import pymorphy2
+import json5
+import math
+
+morph = pymorphy2.MorphAnalyzer()
+
+def compute_tf(words):
+    dictionary = dict()
+    for word in words:
+        if not word in dictionary.keys():
+            dictionary[word] = 1
+        else:
+            dictionary[word] += 1
+    for lemma in dictionary.keys():
+        dictionary[lemma] /= len(words)
+    return dictionary
+
+
+def compute_idf(json):
+    dictionary = dict()
+    for index in json.keys():
+        dictionary[index] = math.log(100/len(json[index]))
+    return dictionary
+
+
+def compute_tfidf(tf, idf):
+    dictionary = dict()
+    for w in tf.keys():
+        dictionary[w] = tf[w] * idf[w]
+    return dictionary
+
+
+file = open(f'../3/index.txt', 'r', encoding='utf-8')
+indexes = file.readlines()
+json = json5.loads("".join(indexes))
+idf = compute_idf(json)
+tfidf = []
+for i in range(1, 101):
+    file = open(f'../2/file/{i}.txt', 'r', encoding='utf-8')
+    lines = file.readlines()
+    words = []
+    for line in lines:
+        word = line.lower().replace('\n', '')
+        p = morph.parse(word)[0].normal_form
+        words.append(p)
+    tf = compute_tf(words)
+    tfidf.append(compute_tfidf(tf,idf))
+str = json5.dumps(tfidf, separators=(',', ':'), sort_keys=False, indent=4, ensure_ascii=False)
+file = open(f'index.txt', 'w', encoding='utf-8')
+file.write(str)
---
+++ 5/search.py
@@ -0,0 +1,107 @@
+import math
+
+import json5
+
+from scipy import spatial
+import pymorphy2
+
+morph = pymorphy2.MorphAnalyzer()
+
+
+def get_indexes(tf_idf_data):
+    all_data = set()
+    for data in tf_idf_data:
+        all_data.update(list(data.keys()))
+    return list(set(all_data))
+
+
+def read_tf_idf_file():
+    with open("../4/index.txt", encoding='utf-8') as file:
+        return json5.load(file)
+
+
+def create_matrix(all_files_words, tf_idf_data):
+    page_count = 100
+    all_count = len(all_files_words)
+    matrix = [[0] * all_count for _ in range(page_count)]
+    for idx, page in enumerate(tf_idf_data):
+        # print(page)
+        page_words = page.keys()
+        for word in page_words:
+            all_idx = all_files_words.index(word)
+            matrix[idx][all_idx] = page[word]
+    # print(sum(matrix[0]))
+    return matrix
+
+
+def get_tf_for_word(word, tokens):
+    return tokens.count(word) / len(tokens)
+
+
+def get_idf_for_word(word, tokens):
+    file = open(f'../3/index.txt', 'r', encoding='utf-8')
+    indexes = file.readlines()
+    json = json5.loads("".join(indexes))
+    return math.log(100 / len(json[word]))
+
+
+def get_tf_idf_for_word(word, tokens):
+    idf = get_idf_for_word(word, tokens)
+    tf = get_tf_for_word(word, tokens)
+    return tf * idf
+
+
+def get_query_vector(query, all_files_words):
+    normal_query = query.lower().split(" ")
+    for i in range(len(normal_query)):
+        normal_query[i] = morph.parse(normal_query[i])[0].normal_form
+    vector = list()
+    for word in all_files_words:
+        if word in normal_query:
+            vector.append(get_tf_idf_for_word(word, normal_query))
+        else:
+            vector.append(0)
+    return vector
+
+
+def get_distances(query_vector, matrix):
+    distances = list()
+    for i, vector in enumerate(matrix):
+        distance = spatial.distance.cosine(query_vector, vector)
+        distance = distance or 1
+        distances.append(distance)
+    return distances
+
+
+def read_sites():
+    with open("../1/index2.txt", 'r', encoding="utf-8") as outfile:
+        return json5.load(outfile)
+
+
+def get_page_url(idx):
+    sites = read_sites()
+    for i in sites:
+        if i["file_name"] == f"{idx}.txt":
+            return i["url"]
+    return ""
+
+
+def convert_distances_to_results(distances):
+    results = list()
+    for i, distance in enumerate(distances):
+        url = get_page_url(i + 1)
+        results.append({"url": url, "distance": distance})
+    return sorted(results, key=lambda result: result["distance"])
+
+
+query = "Прием заявок КФУ"
+tf_idf_data = read_tf_idf_file()
+all_files_words = get_indexes(tf_idf_data)
+matrix = create_matrix(all_files_words, tf_idf_data)
+query_vector = get_query_vector(query, all_files_words)
+distances = get_distances(query_vector, matrix)
+ans = convert_distances_to_results(distances)
+str = json5.dumps(ans, separators=(',', ':'), sort_keys=False, indent=4, ensure_ascii=False)
+file = open(f'index.txt', 'w', encoding='utf-8')
+file.write(str)
+print(len(ans))
